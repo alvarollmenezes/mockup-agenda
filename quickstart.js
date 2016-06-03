0f15ed1151ec10eef8575e4565c3d4c6e8df8e0d
alvarollmenezes <alvarollmenezes@gmail.com>
2016-06-03 17:32:41 -0300

    Teste da api do google utilizando service key.

---
+++ quickstart.js
@@ -0,0 +1,63 @@
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+// If modifying these scopes, delete your previously saved credentials
+// at ~/.credentials/calendar-nodejs-quickstart.json
+var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+authorize();
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    var auth = new googleAuth();
+
+    auth._getApplicationCredentialsFromFilePath('client_secret.json', function (err, authClient) {
+        if (err) {
+            console.log('Authentication failed because of ', err);
+            return;
+        }
+        if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+            authClient = authClient.createScoped(SCOPES);
+            listEvents(authClient);
+        }
+    });
+}
+
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listEvents(auth) {
+    var calendar = google.calendar('v3');
+    calendar.events.list({
+        auth: auth,
+        calendarId: '1r36np3r0u0oui3ar77k8ktadg@group.calendar.google.com',
+        timeMin: (new Date(1990, 01, 01)).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime'
+    }, function (err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var events = response.items;
+        if (events.length == 0) {
+            console.log('No upcoming events found.');
+        } else {
+            console.log('Upcoming 10 events:');
+            for (var i = 0; i < events.length; i++) {
+                var event = events[i];
+                var start = event.start.dateTime || event.start.date;
+                console.log('%s - %s', start, event.summary);
+            }
+        }
+    });
+}
