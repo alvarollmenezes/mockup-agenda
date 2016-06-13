cde489e13d197637be08ee8eead076c66131a2fc
alvarollmenezes <alvarollmenezes@gmail.com>
2016-06-13 15:56:15 -0300

    ...

--- app.js
+++ app.js
@@ -30,9 +30,17 @@ app.get('/events', function (req, res) {
     res.set("Content-Type", "application/json");
 
     var secrets = require('./agendas.json');
-
     var params = req.query;
-    var agendas = JSON.parse(params.agendas);
+
+    var agendas;
+    if (!Array.isArray(params.agendas)) {
+        agendas = [params.agendas];
+    }
+    else {
+        agendas = params.agendas;
+    }
+    // Remove duplicates
+    agendas = Array.from(new Set(agendas));
 
     var promises = agendas.map(function (agenda) {
         var cal = secrets[agenda];
@@ -42,7 +50,7 @@ app.get('/events', function (req, res) {
     Promise.all(promises).then(function (eventos) {
         eventosComCor = eventos.map(colorirAgenda);
 
-        res.send(eventosComCor);
+        res.json(eventosComCor);
 
     }).catch(function (err) {
         console.log(err);
@@ -57,7 +65,7 @@ app.get('/agendas', function (req, res) {
 
     var agendas = Object.keys(secrets).map(key => { return { nome: key, color: gerarCor(key) } });
 
-    res.send(agendas);
+    res.json(agendas);
 });
 
 // Launch server
