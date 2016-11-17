7d82a2e10d83c0af7b20a5d4ec940434fe476d88
alvarollmenezes <alvarollmenezes@gmail.com>
2016-11-17 13:08:31 -0200

    Parametriza as cores

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    apiKey: process.env.API_KEY,
-    colors: [ '#8BC34A', '#03A9F4', '#FFEB3B', '#FF5722', '#9C27B0', '#795548', '#E91E63', '#3F51B5', '#F44336', '#009688',
-                     '#2196F3', '#00BCD4', '#673AB7', '#4CAF50', '#DCE77', '#FFF9C4', '#FB8C00', '#FFB300', '#607D8B' ]
+    apiKey: process.env.API_KEY || 'google-key',
+    colors: JSON.parse( process.env.COLORS ) || [ '#8E2E11', '#280C54', '#004766', '#456600', '#966900' ]
 };
